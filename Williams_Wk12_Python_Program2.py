513520a66dc4205ca066aa5be1b3d876417ea933
Seth J. Williams <sjwilliams3@students.unwsp.edu>
2024-11-23 12:16:23 -0600

    Update Williams_Wk12_Python_Program2.py
    Coding contains errors, checks all items with one click, and does not produce any prices.

--- Williams_Wk12_Python_Program2.py
+++ Williams_Wk12_Python_Program2.py
@@ -12,18 +12,13 @@
 import tkinter
 import tkinter.messagebox
 
-class CarService_GUI:
+class MyGUI:
     def __init__(self):
-        # Create the main window.
         self.main_window = tkinter.Tk()
 
-        # Create two frames. One for the check buttons
-        # and another for teh regular Button widgets
         self.top_frame = tkinter.Frame(self.main_window)
         self.bottom_frame = tkinter.Frame(self.main_window)
 
-        # Create seven IntVar objects to use with
-        # the check buttons
         self.cb_var1 = tkinter.IntVar()
         self.cb_var2 = tkinter.IntVar()
         self.cb_var3 = tkinter.IntVar()
@@ -32,7 +27,6 @@ class CarService_GUI:
         self.cb_var6 = tkinter.IntVar()
         self.cb_var7 = tkinter.IntVar()
 
-        # Set IntVar object prices
         self.cb_var1.set(0)
         self.cb_var2.set(0)
         self.cb_var3.set(0)
@@ -41,30 +35,35 @@ class CarService_GUI:
         self.cb_var6.set(0)
         self.cb_var7.set(0)
 
-        # Create check button widgets in top_frame
         self.cb1 = tkinter.Checkbutton(self.top_frame,
                                        text='Oil Change - $30.00',
-                                       variable=self.cb_var1)
+                                       variable=self.cb_var1,
+                                       command=self.show_choice)
         self.cb2 = tkinter.Checkbutton(self.top_frame,
                                        text='Lube Job - $20.00',
-                                       variable=self.cb_var2)
+                                       variable=self.cb_var1,
+                                       command=self.show_choice)
         self.cb3 = tkinter.Checkbutton(self.top_frame,
                                        text='Radiator Flush - $40.00',
-                                       variable=self.cb_var3)
+                                       variable=self.cb_var1,
+                                       command=self.show_choice)
         self.cb4 = tkinter.Checkbutton(self.top_frame,
                                        text='Transmission Fluid - $100.00',
-                                       variable=self.cb_var4)
+                                       variable=self.cb_var1,
+                                       command=self.show_choice)
         self.cb5 = tkinter.Checkbutton(self.top_frame,
                                        text='Inspection - $35.00',
-                                       variable=self.cb_var5)
+                                       variable=self.cb_var1,
+                                       command=self.show_choice)
         self.cb6 = tkinter.Checkbutton(self.top_frame,
                                        text='Muffler replacement - $200.00',
-                                       variable=self.cb_var6)
+                                       variable=self.cb_var1,
+                                       command=self.show_choice)
         self.cb7 = tkinter.Checkbutton(self.top_frame,
                                        text='Tire Rotation - $20.00',
-                                       variable=self.cb_var7)
+                                       variable=self.cb_var1,
+                                       command=self.show_choice)
 
-        # Pack Checkbuttons
         self.cb1.pack()
         self.cb2.pack()
         self.cb3.pack()
@@ -73,7 +72,6 @@ class CarService_GUI:
         self.cb6.pack()
         self.cb7.pack()
 
-        # Create a Get Price button and a Quit button
         self.getprice_button = tkinter.Button(self.bottom_frame,
                                               text='Get Price',
                                               command=self.show_choice)
@@ -81,42 +79,35 @@ class CarService_GUI:
                                           text='Quit',
                                           command=self.main_window.destroy)
 
-        # Pack buttons
         self.getprice_button.pack(side='left')
         self.quit_button.pack(side='left')
 
-        # Pack frames
         self.top_frame.pack()
         self.bottom_frame.pack()
 
-        # Start mainloop
         tkinter.mainloop()
 
-    # show_choice method is callback for Get Price button
-
     def show_choice(self):
-        # Create a message string
-        self.message = 'Your total:\n'
-
-        # Determine which checkbuttons are selected and
-        # build the message string accordingly.
         total = 0
-        if self.cb_var1.get() == 1:
-            self.message = self.message + '1\n'
-        if self.cb_var2.get() == 1:
-            self.message = self.message + '2\n'
-        if self.cb_var3.get() == 1:
-            self.message = self.message + '3\n'
-        if self.cb_var4.get() == 1:
-            self.message = self.message + '4\n'
-        if self.cb_var5.get() == 1:
-            self.message = self.message + '5\n'
-        if self.cb_var6.get() == 1:
-            self.message = self.message + '6\n'
-        if self.cb_var7.get() == 1:
-            self.message = self.message + '7\n'
+        if self.var1.get() == 1:
+            total += 30
+        if self.var2.get() == 1:
+            total += 20
+        if self.var3.get() == 1:
+            total += 40
+        if self.var4.get() == 1:
+            total += 100
+        if self.var5.get() == 1:
+            total += 35
+        if self.var6.get() == 1:
+            total += 200
+        if self.var7.get() == 1:
+            total += 20
+        self.message = f"Your total: ${total}"
 
-            tkinter.messagebox.showinfo('Total', self.message)
+        self.message.pack()
+
+        tkinter.messagebox.showinfo('Total Charges', self.message)
 
 if __name__ == '__main__':
-    carservice_gui = CarService_GUI()
+    my_gui = MyGUI()
