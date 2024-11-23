49f0cc132daf341718b7606fb9ca4146fdca1ab4
ljsnell <ljsnell@users.noreply.github.com>
2024-11-23 17:01:18 -0600

    Update Williams_Wk12_Python_Program3.py

--- Williams_Wk12_Python_Program3.py
+++ Williams_Wk12_Python_Program3.py
@@ -18,9 +18,9 @@ class MyGUI:
         self.mid_frame = tkinter.Frame(self.main_window)
         self.bottom_frame = tkinter.Frame(self.main_window)
 
-        self.radio_var = tkinter.IntVar()
+        self.radio_var = tkinter.DoubleVar()
 
-        self.radio_var.set(0)
+        self.radio_var.set(11)
 
         self.rb1 = tkinter.Radiobutton(self.top_frame,
                                        text='Daytime (6:00 A.M. through 5:59 P.M): $0.02',
@@ -67,12 +67,14 @@ class MyGUI:
 
     def show_choice(self):
         total = 0
-        if self.rb1.get() == 1:
-            total = self.time_entry * 0.02
-        if self.rb2.get() == 1:
-            total = self.time_entry * 0.12
-        if self.rb3.get() == 1:
-            total = self.time_entry * 0.05
+        print(self.radio_var.get())
+        total = self.time_entry * self.radio_var.get()
+        # if self.rb1.get() == 1:
+        #     total = self.time_entry * 0.02
+        # if self.rb2.get() == 1:
+        #     total = self.time_entry * 0.12
+        # if self.rb3.get() == 1:
+        #     total = self.time_entry * 0.05
         tkinter.messagebox.showinfo('Selection',f'Your charges: ${total}')
 
 if __name__ == '__main__':
